683a2ea90ceaf981736ee8db5dba8ef705dbcab7
meddingt <meddingt@bf843fa0-5fda-4187-82bf-2edb81848ee6>
2009-10-09 08:14:15 +0000

    git-svn-id: svn://svn.code.sf.net/p/peachfuzz/svn/branches/Peach2.3@1854 bf843fa0-5fda-4187-82bf-2edb81848ee6

--- dependencies/py2.5-win32/noguipcap.bat
+++ dependencies/py2.5-win32/noguipcap.bat
@@ -2,11 +2,11 @@
 
 msiexec /qn /i 4Suite-XML-1.0.2.win32-py2.5.msi
 msiexec /qn /i cDeepCopy-0.2.win32-py2.5.msi
-msiexec /qn /i comtypes-0.4.2.win32-py2.5.msi
+msiexec /qn /i comtypes-0.6.1.win32-py2.5.msi
 msiexec /qn /i cPeach-0.2.win32-py2.5.msi
 msiexec /qn /i pyasn1-0.0.7a.win32-py2.5.msi
 msiexec /qn /i pyvmware-0.1.win32-py2.5.msi
 msiexec /qn /i pcap-peach.win32-py2.5.msi
 msiexec /qn /i zope.interface-3.3.0.win32-py2.5.msi
 msiexec /qn /i Twisted-8.0.1.win32-py2.5.msi
-msiexec /qn /i PyDbgEng-0.10.win32-py2.5.msi
+msiexec /qn /i PyDbgEng-0.11.win32-py2.5.msi
