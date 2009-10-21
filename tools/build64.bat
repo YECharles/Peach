aa175c4ac9af75a4350f7d133d41da8d1ebd70d7
meddingt <meddingt@bf843fa0-5fda-4187-82bf-2edb81848ee6>
2009-10-21 13:13:25 +0000

    git-svn-id: svn://svn.code.sf.net/p/peachfuzz/svn/branches/Peach2.3@1868 bf843fa0-5fda-4187-82bf-2edb81848ee6

---
+++ tools/build64.bat
@@ -0,0 +1,61 @@
+@echo off
+
+rem Build the binary version of Peach using py2exe
+rem Copyright (c) Michael Eddington
+
+cd c:\peach\tools
+
+copy /y setup.py ..
+
+cd c:\peach
+
+rmdir /s/q bin
+rmdir /s/q build
+rmdir /s/q dist
+
+mkdir build\bdist.win-amd64-2.6\msi\Share\4Suite\Schemata
+copy C:\Python26\Share\4Suite\default.cat build\bdist.win-amd64-2.6\msi\Share\4Suite
+copy C:\Python26\Share\4Suite\Schemata\*.* build\bdist.win-amd64-2.6\msi\Share\4Suite\Schemata
+
+rem -O0 will cause optmized byte code to
+rem be generated
+c:\python26\python -OO setup.py py2exe
+
+ren dist bin
+rmdir /s/q build
+del /q setup.py
+
+rem Build DbgProxyEvent
+rem cd c:\peach\dependencies\src\peach-PyDbgEng
+rem rmdir /s/q build dist
+rem python py2exe\setup.py py2exe
+rem copy /y dist\* c:\peach\bin
+rem cd c:\peach
+
+rem These extra files not needed on x64 yet
+rem Extra re-dist files
+rem copy C:\Python25\lib\site-packages\wx-2.8-msw-ansi\wx\gdiplus.dll bin
+rem copy C:\Python25\lib\site-packages\wx-2.8-msw-ansi\wx\MSVCP71.dll bin
+rem copy c:\windows\system32\MFC71.DLL bin
+rem copy c:\windows\SysWOW64\mfc71.dll bin
+
+xcopy /s/q c:\peach\Peach\Generators\xmltests c:\peach\bin\xmltests\
+
+if "%1"==all call c:\peach\tools\gendocs.bat
+if "%1"==bin goto BINONLY
+
+goto EXIT
+
+:BINONLY
+
+cd c:\peach
+copy /y C:\peach\Peach\Engine\PeachTypes.xml
+rmdir /s/q dependencies
+rmdir /s/q Peach
+rmdir /s/q test
+del /y *.py
+del /y *.pyw
+
+:EXIT
+
+rem ALl done!
