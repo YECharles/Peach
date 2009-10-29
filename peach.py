2b4f56dd5babf3f9be4681144a875e07bf347b92
meddingt <meddingt@bf843fa0-5fda-4187-82bf-2edb81848ee6>
2009-10-29 23:39:01 +0000

    git-svn-id: svn://svn.code.sf.net/p/peachfuzz/svn/branches/Peach2.3@1888 bf843fa0-5fda-4187-82bf-2edb81848ee6

--- peach.py
+++ peach.py
@@ -54,11 +54,21 @@ except:
 	print "downloaded from http://4suite.org/.\n\n"
 	sys.exit(0)
 
-def printError(str):
-	sys.stderr.write("%s\n" % str)
 
-def usage():
-	printError("""
+# Note: this is required for multiprocessing module to work.
+if __name__ == '__main__':
+	
+	try:
+		from multiprocessing import freeze_support
+		freeze_support()
+	except:
+		pass
+
+	def printError(str):
+		sys.stderr.write("%s\n" % str)
+	
+	def usage():
+		printError("""
 This is the Peach Runtime.  The Peach Runtime is one of the many ways
 to use Peach XML files.  Currently this runtime is still in development
 but already exposes several abilities to the end-user such as performing
@@ -82,12 +92,12 @@ Syntax:
   -c,--count                 Count test cases
   -t,--test xml_file         Test parse a Peach XML file
   -p,--parallel M,N          Parallel fuzzing.  Total of M machines, this
-                             is machine N.
+							 is machine N.
   --debug                    Enable debug messages. Usefull when debugging
-                             your Peach XML file.  Warning: Messages are very
-                             cryptic sometimes.
+							 your Peach XML file.  Warning: Messages are very
+							 cryptic sometimes.
   --skipto N                 Skip to a specific test #.  This replaced -r
-                             for restarting a Peach run.
+							 for restarting a Peach run.
   --range N,M                Provide a range of test #'s to be run.
 
 Peach Agent
@@ -142,355 +152,355 @@ Debug Peach XML File
   debugging as well.
 
 """)
-	sys.exit(0)
-
-printError("\n] Peach 2.3.2 DEV Runtime")
-printError("] Copyright (c) Michael Eddington\n")
-
-if sys.version[:3] not in ['2.5', '2.6']:
-	printError("Error: Peach requires Python v2.5 or v2.6.")
-	sys.exit(0)
-
-noCount = False
-verbose = False
-webWatcher = False
-restartFuzzer = False
-restartFuzzerFile = None
-parallel = None
-startNum = None
-
-
-# Add our root to the python path
-if not (hasattr(sys,"frozen") and sys.frozen == "console_exe"):
-	sys.path.append(sys.path[0])
-else:
-	p = os.path.dirname(os.path.abspath(sys.executable))
-	sys.path.append(p)
-	sys.path.append( os.path.normpath( os.path.join(p, "..") ) )
-
-
-try:
-	(optlist, args) = getopt.getopt(sys.argv[1:], "p:vstcwagr:1", ['strategy=','analyzer=', 'parallel=',
-																 'restart=', 'parser=',
-																 'test', 'count', 'web', 'agent',
-																 'gui', 'debug', 'new', 'skipto=',
-																 'range'])
-except:
-	usage()
-
-if len(optlist) < 1 and len(args) < 1:
-	usage()
-
-for i in range(len(optlist)):
-	if optlist[i][0] == '--analyzer':
-		
-		# set the analyzer to use
-		
-		try:	
-			analyzer = optlist[i][1]
-			if analyzer == None or len(analyzer) == 0:
-				analyzer = args[0]
-			
-			from Peach.Engine.common import *
-			from Peach.Analyzers import *
-			
-			cls = eval(analyzer)
-			if cls.supportCommandLine:
-				print "[*] Using %s as analyzer" % analyzer
-				
-				cls = eval("%s()" % analyzer)
-				
-				a = {}
-				
-				for i in range(0, len(args), 2):
-					a[args[i]] = args[i+1]
-				
-				cls.asCommandLine(a)
-			
-			elif cls.supportParser:
-				print "[*] Using %s as parser" % analyzer
-				Analyzer.DefaultParser = cls
-		
-		except PeachException, pe:
-			print ""
-			print pe.msg, "\n"
-			
 		sys.exit(0)
 	
-	elif optlist[i][0] == '--parser':
-		
-		# set the analyzer to use
-		
-		try:	
-			analyzer = optlist[i][1]
-			if analyzer == None or len(analyzer) == 0:
-				analyzer = args[0]
-			
-			from Peach.Engine.common import *
-			from Peach.Analyzers import *
-			
-			cls = eval(analyzer)
-			if cls.supportParser:
-				print "[*] Using %s as parser" % analyzer
-				Analyzer.DefaultParser = cls
-			
-			else:
-				raise PeachException("Error: Analyzer %s does not support being a parser!" % analyzer)
-		
-		except PeachException, pe:
-			print ""
-			print pe.msg, "\n"
-			
+	printError("\n] Peach 2.3.2 Runtime")
+	printError("] Copyright (c) Michael Eddington\n")
+	
+	if sys.version[:3] not in ['2.5', '2.6']:
+		printError("Error: Peach requires Python v2.5 or v2.6.")
 		sys.exit(0)
 	
-	elif optlist[i][0] == '--strategy':
-		
-		# Set the fuzzing strategy to use
-		
-		try:	
-			strategy = optlist[i][1]
-			if strategy == None or len(strategy) == 0:
-				strategy = args[0]
-			
-			from Peach.Engine.common import *
-			from Peach.mutatestrategies import *
-			from Peach.MutateStrategies import *
-			
-			exec("MutationStrategy.DefaultStrategy = %s" % strategy)
-		
-		except:
-			print ""
-			print "Error using mutation strategy '%s'.\n" % strategy
-			#raise
-			sys.exit(0)
+	noCount = False
+	verbose = False
+	webWatcher = False
+	restartFuzzer = False
+	restartFuzzerFile = None
+	parallel = None
+	startNum = None
 	
-	elif optlist[i][0] == '--debug':
-		
-		# show debugging messages
-		
-		from Peach.Engine import *
-		from Peach.Engine.common import *
-		
-		engine.Engine.debug = True
-		
-	elif optlist[i][0] == '--new':
-		
-		# use the new match relation stuffs
-		
-		from Peach.Engine import *
-		from Peach.Engine.common import *
-		
-		engine.Engine.relationsNew = True
-		
-	elif optlist[i][0] == '-1':
-		
-		# use the new match relation stuffs
-		
-		from Peach.Engine import *
-		from Peach.Engine.common import *
-		
-		print "[*] Performing single iteration"
-		engine.Engine.justOne = True
-		
-	elif optlist[i][0] == '--range':
-		
-		# use the new match relation stuffs
-		
-		from Peach.Engine import *
-		from Peach.Engine.common import *
-		
-		try:
-			range = optlist[i][1].split(',')
-			range[0] = long(range[0])
-			range[1] = long(range[1])
-		except:
-			range = [long(args[0]), long(args[1])]
-			args = args[2:]
-		
-		if range[0] < 0:
-			print "Error: Count must be positive."
+	
+	# Add our root to the python path
+	if not (hasattr(sys,"frozen") and sys.frozen == "console_exe"):
+		sys.path.append(sys.path[0])
+	else:
+		p = os.path.dirname(os.path.abspath(sys.executable))
+		sys.path.append(p)
+		sys.path.append( os.path.normpath( os.path.join(p, "..") ) )
+	
+	
+	try:
+		(optlist, args) = getopt.getopt(sys.argv[1:], "p:vstcwagr:1", ['strategy=','analyzer=', 'parallel=',
+																	 'restart=', 'parser=',
+																	 'test', 'count', 'web', 'agent',
+																	 'gui', 'debug', 'new', 'skipto=',
+																	 'range'])
+	except:
+		usage()
+	
+	if len(optlist) < 1 and len(args) < 1:
+		usage()
+	
+	for i in range(len(optlist)):
+		if optlist[i][0] == '--analyzer':
+			
+			# set the analyzer to use
+			
+			try:	
+				analyzer = optlist[i][1]
+				if analyzer == None or len(analyzer) == 0:
+					analyzer = args[0]
+				
+				from Peach.Engine.common import *
+				from Peach.Analyzers import *
+				
+				cls = eval(analyzer)
+				if cls.supportCommandLine:
+					print "[*] Using %s as analyzer" % analyzer
+					
+					cls = eval("%s()" % analyzer)
+					
+					a = {}
+					
+					for i in range(0, len(args), 2):
+						a[args[i]] = args[i+1]
+					
+					cls.asCommandLine(a)
+				
+				elif cls.supportParser:
+					print "[*] Using %s as parser" % analyzer
+					Analyzer.DefaultParser = cls
+			
+			except PeachException, pe:
+				print ""
+				print pe.msg, "\n"
+				
 			sys.exit(0)
 		
-		if range[0] >= range[1]:
-			print "Error: Range must be 1 or larger."
+		elif optlist[i][0] == '--parser':
+			
+			# set the analyzer to use
+			
+			try:	
+				analyzer = optlist[i][1]
+				if analyzer == None or len(analyzer) == 0:
+					analyzer = args[0]
+				
+				from Peach.Engine.common import *
+				from Peach.Analyzers import *
+				
+				cls = eval(analyzer)
+				if cls.supportParser:
+					print "[*] Using %s as parser" % analyzer
+					Analyzer.DefaultParser = cls
+				
+				else:
+					raise PeachException("Error: Analyzer %s does not support being a parser!" % analyzer)
+			
+			except PeachException, pe:
+				print ""
+				print pe.msg, "\n"
+				
+			sys.exit(0)
+		
+		elif optlist[i][0] == '--strategy':
+			
+			# Set the fuzzing strategy to use
+			
+			try:	
+				strategy = optlist[i][1]
+				if strategy == None or len(strategy) == 0:
+					strategy = args[0]
+				
+				from Peach.Engine.common import *
+				from Peach.mutatestrategies import *
+				from Peach.MutateStrategies import *
+				
+				exec("MutationStrategy.DefaultStrategy = %s" % strategy)
+			
+			except:
+				print ""
+				print "Error using mutation strategy '%s'.\n" % strategy
+				#raise
+				sys.exit(0)
+		
+		elif optlist[i][0] == '--debug':
+			
+			# show debugging messages
+			
+			from Peach.Engine import *
+			from Peach.Engine.common import *
+			
+			engine.Engine.debug = True
+			
+		elif optlist[i][0] == '--new':
+			
+			# use the new match relation stuffs
+			
+			from Peach.Engine import *
+			from Peach.Engine.common import *
+			
+			engine.Engine.relationsNew = True
+			
+		elif optlist[i][0] == '-1':
+			
+			# use the new match relation stuffs
+			
+			from Peach.Engine import *
+			from Peach.Engine.common import *
+			
+			print "[*] Performing single iteration"
+			engine.Engine.justOne = True
+			
+		elif optlist[i][0] == '--range':
+			
+			# use the new match relation stuffs
+			
+			from Peach.Engine import *
+			from Peach.Engine.common import *
+			
+			try:
+				range = optlist[i][1].split(',')
+				range[0] = long(range[0])
+				range[1] = long(range[1])
+			except:
+				range = [long(args[0]), long(args[1])]
+				args = args[2:]
+			
+			if range[0] < 0:
+				print "Error: Count must be positive."
+				sys.exit(0)
+			
+			if range[0] >= range[1]:
+				print "Error: Range must be 1 or larger."
+				sys.exit(0)
+				
+			print "[*] Performing tests %d -> %d" % (range[0], range[1])
+			engine.Engine.testRange = range
+			
+		elif optlist[i][0] == '--test' or optlist[i][0] == '-t':
+	
+			# do a parse test
+			
+			from Peach.Engine import *
+			from Peach.Engine.common import *
+			
+			try:
+				
+				# do the test
+				if args[0][:5] != 'file:':
+					args[0] = 'file:' + args[0]
+				
+				from Peach.analyzer import Analyzer
+				
+				parser = Analyzer.DefaultParser()
+				parser.asParser(args[0])
+				
+				print "File parsed with out errors.\n\n"
+	
+			except PeachException, pe:
+				print ""
+				print pe.msg
+			
+			except Ft.Xml.ReaderException, e:
+				print ""
+				print "An error occured parsing the XML file\n" + str(e)
+			
+			except:
+				raise
+			
 			sys.exit(0)
 			
-		print "[*] Performing tests %d -> %d" % (range[0], range[1])
-		engine.Engine.testRange = range
-		
-	elif optlist[i][0] == '--test' or optlist[i][0] == '-t':
-
-		# do a parse test
-		
-		from Peach.Engine import *
-		from Peach.Engine.common import *
-		
-		try:
+		elif optlist[i][0] == '--count' or optlist[i][0] == '-c':
 			
-			# do the test
+			# count the total test case #
+			
+			from Peach.Engine import *
+			from Peach.Engine.common import *
+			
 			if args[0][:5] != 'file:':
 				args[0] = 'file:' + args[0]
 			
-			from Peach.analyzer import Analyzer
+			engine = engine.Engine()
+			if len(args) > 1:
+				engine.Count(args[0], args[1])
+			else:
+				engine.Count(args[0], None)
 			
-			parser = Analyzer.DefaultParser()
-			parser.asParser(args[0])
+			sys.exit(0)
 			
-			print "File parsed with out errors.\n\n"
-
-		except PeachException, pe:
-			print ""
-			print pe.msg
+		elif optlist[i][0] == '--gui' or optlist[i][0] == '-g':
+			
+			# Start peach builder UI
+			
+			from Peach.Engine import *
+			from Peach.Engine.common import *
+	
+			import os
+			from Peach.Gui import PeachGui
+	
+			os.chdir( sys.path[0] + "/peach/gui" )
+	
+			PeachGui.RunPeachEditor()
+			sys.exit(0)
 		
-		except Ft.Xml.ReaderException, e:
-			print ""
-			print "An error occured parsing the XML file\n" + str(e)
+		elif optlist[i][0] == '-w' or optlist[i][0] == '--web':
+			
+			# enable web watcher
+			
+			webWatcher = True
+			
+		elif optlist[i][0] == '-r' or optlist[i][0] == '--restart':
+			
+			# Restarting a fuzzer run
+			
+			print "Error, -r/--restart is no longer supported.  Instead say \"--skipto N\" where N is the test count."
+			sys.exit(0)
+			
+		elif optlist[i][0] == '--skipto':
+			
+			# Skip to a specific test #
+			
+			startNum = int(optlist[i][1])
+			
+		elif optlist[i][0] == '-p' or optlist[i][0] == '--parallel':
+			
+			# Parallel fuzzing run
+			
+			try:
+				parallel = optlist[i][1].split(',')
+				parallel[0] = long(parallel[0])
+				parallel[1] = long(parallel[1])
+			except:
+				parallel = [long(optlist[i][1]), long(args[0])]
+				args = args[1:]
+			
+			if parallel[0] < 1:
+				print "Error: Machine count must be >= 2."
+				sys.exit(0)
+			
+			if parallel[0] <= parallel[1]:
+				print "Error: When performing parallel fuzzing run, the total number of machines must be less than the current machine."
+				sys.exit(0)
+			
+			print "-- Parallel total machines: %d" % parallel[0]
+			print "-- Parallel our machine...: %d\n" % parallel[1]
+			
+		elif optlist[i][0] == '-a' or optlist[i][0] == '--agent':
+			
+			from Peach.Engine import *
+			from Peach.Engine.common import *
+			
+			# start a peach agent
+			port = 9000
+			password = None
+			try:
+				port = int(args[0])
+				if len(args[1]) > 0:
+					password = args[1]
+			except:
+				pass
+			
+			from Peach.agent import Agent
+			agent = Agent(password, port)
+			sys.exit(0)
+			
+		elif optlist[i][0] == '-v':
+			verbose = True
 		
-		except:
-			raise
-		
-		sys.exit(0)
-		
-	elif optlist[i][0] == '--count' or optlist[i][0] == '-c':
-		
-		# count the total test case #
-		
-		from Peach.Engine import *
-		from Peach.Engine.common import *
-		
-		if args[0][:5] != 'file:':
-			args[0] = 'file:' + args[0]
-		
-		engine = engine.Engine()
-		if len(args) > 1:
-			engine.Count(args[0], args[1])
 		else:
-			engine.Count(args[0], None)
-		
-		sys.exit(0)
-		
-	elif optlist[i][0] == '--gui' or optlist[i][0] == '-g':
-		
-		# Start peach builder UI
-		
-		from Peach.Engine import *
-		from Peach.Engine.common import *
-
-		import os
-		from Peach.Gui import PeachGui
-
-		os.chdir( sys.path[0] + "/peach/gui" )
-
-		PeachGui.RunPeachEditor()
-		sys.exit(0)
+			usage()
 	
-	elif optlist[i][0] == '-w' or optlist[i][0] == '--web':
-		
-		# enable web watcher
-		
-		webWatcher = True
-		
-	elif optlist[i][0] == '-r' or optlist[i][0] == '--restart':
-		
-		# Restarting a fuzzer run
-		
-		print "Error, -r/--restart is no longer supported.  Instead say \"--skipto N\" where N is the test count."
-		sys.exit(0)
-		
-	elif optlist[i][0] == '--skipto':
-		
-		# Skip to a specific test #
-		
-		startNum = int(optlist[i][1])
-		
-	elif optlist[i][0] == '-p' or optlist[i][0] == '--parallel':
-		
-		# Parallel fuzzing run
-		
-		try:
-			parallel = optlist[i][1].split(',')
-			parallel[0] = long(parallel[0])
-			parallel[1] = long(parallel[1])
-		except:
-			parallel = [long(optlist[i][1]), long(args[0])]
-			args = args[1:]
-		
-		if parallel[0] < 1:
-			print "Error: Machine count must be >= 2."
-			sys.exit(0)
-		
-		if parallel[0] <= parallel[1]:
-			print "Error: When performing parallel fuzzing run, the total number of machines must be less than the current machine."
-			sys.exit(0)
-		
-		print "-- Parallel total machines: %d" % parallel[0]
-		print "-- Parallel our machine...: %d\n" % parallel[1]
-		
-	elif optlist[i][0] == '-a' or optlist[i][0] == '--agent':
-		
-		from Peach.Engine import *
-		from Peach.Engine.common import *
-		
-		# start a peach agent
-		port = 9000
-		password = None
-		try:
-			port = int(args[0])
-			if len(args[1]) > 0:
-				password = args[1]
-		except:
-			pass
-		
-		from Peach.agent import Agent
-		agent = Agent(password, port)
-		sys.exit(0)
-		
-	elif optlist[i][0] == '-v':
-		verbose = True
+	from Peach.Engine import *
+	from Peach.Engine.common import *
 	
-	else:
-		usage()
-
-from Peach.Engine import *
-from Peach.Engine.common import *
-
-engine = engine.Engine()
-watcher = None
-
-try:
+	engine = engine.Engine()
+	watcher = None
+	
 	try:
-		if args[0][:5] != 'file:':
-			args[0] = 'file:' + args[0]
+		try:
+			if args[0][:5] != 'file:':
+				args[0] = 'file:' + args[0]
+		except:
+			raise PeachException("Error, did you supply the Peach Pit file?")
+		
+		if webWatcher == True:
+			from Peach.Engine.webwatcher import PeachWebWatcher
+			watcher = PeachWebWatcher()
+		
+		if len(args) > 1:
+			engine.Run(args[0], args[1], verbose, watcher, restartFuzzerFile, noCount, parallel, startNum)
+		
+		else:
+			engine.Run(args[0], None, verbose, watcher, restartFuzzerFile, noCount, parallel, startNum)
+	
+	except PeachException, pe:
+		print ""
+		print pe.msg
+	
+	except Ft.Xml.ReaderException, e:
+		print ""
+		print "An error occured parsing the XML file\n" + str(e)
+	
 	except:
-		raise PeachException("Error, did you supply the Peach Pit file?")
+		raise
 	
-	if webWatcher == True:
-		from Peach.Engine.webwatcher import PeachWebWatcher
-		watcher = PeachWebWatcher()
-	
-	if len(args) > 1:
-		engine.Run(args[0], args[1], verbose, watcher, restartFuzzerFile, noCount, parallel, startNum)
-	
-	else:
-		engine.Run(args[0], None, verbose, watcher, restartFuzzerFile, noCount, parallel, startNum)
-
-except PeachException, pe:
-	print ""
-	print pe.msg
-
-except Ft.Xml.ReaderException, e:
-	print ""
-	print "An error occured parsing the XML file\n" + str(e)
-
-except:
-	raise
-
-finally:
-	if DomBackgroundCopier.copyThread != None:
-		DomBackgroundCopier.stop.set()
-		DomBackgroundCopier.needcopies.set()
-		DomBackgroundCopier.copyThread.join()
-		DomBackgroundCopier.copyThread = None
+	finally:
+		if DomBackgroundCopier.copyThread != None:
+			DomBackgroundCopier.stop.set()
+			DomBackgroundCopier.needcopies.set()
+			DomBackgroundCopier.copyThread.join()
+			DomBackgroundCopier.copyThread = None
 
 	
 # end
