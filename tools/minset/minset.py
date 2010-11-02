e079c1388be3aeba11d2d440cdf2f76b587eb61e
meddingt <meddingt@bf843fa0-5fda-4187-82bf-2edb81848ee6>
2010-11-02 20:26:59 +0000

    git-svn-id: svn://svn.code.sf.net/p/peachfuzz/svn/branches/Peach2.3@2197 bf843fa0-5fda-4187-82bf-2edb81848ee6

--- tools/minset/minset.py
+++ tools/minset/minset.py
@@ -40,11 +40,16 @@ Currently WinDbg is required along with pydbgeng and comtypes.
 
 # $Id$
 
-import sys, os, threading, glob, re, time, shutil
-import win32api, win32con, win32process, win32pdh
-
-import ctypes
+import sys
+import os
+import glob
+import shutil
+import tempfile
+import win32api
 import win32con
+import win32process
+import win32pdh
+import ctypes
 
 TH32CS_SNAPPROCESS = 0x00000002
 class PROCESSENTRY32(ctypes.Structure):
@@ -60,265 +65,400 @@ class PROCESSENTRY32(ctypes.Structure):
 		("dwFlags", ctypes.c_ulong),
 		("szExeFile", ctypes.c_char * 260)]
 
-def getProcessEntry(pid):
-	# See http://msdn2.microsoft.com/en-us/library/ms686701.aspx
-	CreateToolhelp32Snapshot = ctypes.windll.kernel32.CreateToolhelp32Snapshot
-	Process32First = ctypes.windll.kernel32.Process32First
-	Process32Next = ctypes.windll.kernel32.Process32Next
-	CloseHandle = ctypes.windll.kernel32.CloseHandle
+class PeachCoverage:
 	
-	hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0)
+	def __init__(self):
+		self._traceFolder = None
+		if not self.isWindows():
+			raise Exception("This tool only supports Windows for now.")
 	
-	pe32 = PROCESSENTRY32()
-	pe32.dwSize = ctypes.sizeof(PROCESSENTRY32)
-	
-	if Process32First(hProcessSnap, ctypes.byref(pe32)) == win32con.FALSE:
-		print >> sys.stderr, "Failed getting first process."
-		return
-	
-	while True:
-		if pe32.th32ProcessID == pid:
-			break
-		
-		if Process32Next(hProcessSnap, ctypes.byref(pe32)) == win32con.FALSE:
-			pe32 = None
-			break
-	
-	CloseHandle(hProcessSnap)
-	
-	return pe32
-
-def process_list(parentId):
-	# See http://msdn2.microsoft.com/en-us/library/ms686701.aspx
-	CreateToolhelp32Snapshot = ctypes.windll.kernel32.CreateToolhelp32Snapshot
-	Process32First = ctypes.windll.kernel32.Process32First
-	Process32Next = ctypes.windll.kernel32.Process32Next
-	CloseHandle = ctypes.windll.kernel32.CloseHandle
-	
-	hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0)
-	
-	pe32 = PROCESSENTRY32()
-	pe32.dwSize = ctypes.sizeof(PROCESSENTRY32)
-	
-	if Process32First(hProcessSnap, ctypes.byref(pe32)) == win32con.FALSE:
-		print >> sys.stderr, "Failed getting first process."
-		return
-	
-	while True:
-		if pe32.th32ParentProcessID == parentId:
-			yield pe32.th32ProcessID
-		
-		if Process32Next(hProcessSnap, ctypes.byref(pe32)) == win32con.FALSE:
-			break
-	
-	CloseHandle(hProcessSnap)
-
-def isWindows():
-	if sys.platform == 'win32':
-		return True
-	
-	return False
-
-def getProcessCpuTime(pid):
-	'''
-	Get current process time for PID.
-	'''
-	
-	return getProcessCpuTimeWindows(pid)
-		
-
-#def getProcessCpuTimeUnix(pid):
-#	os.system("ps -o pcpu %d > .cpu" % self.pid)
-#	fd = open(".cpu", "rb")
-#	data = fd.read()
-#	fd.close()
-#	self.unlink(".cpu")
-#	
-#	cpu = re.search(r"\s*(\d+\.\d+)", data).group(1)
-#	
-#	return float(cpu)
-
-def getProcessCpuTimeWindows(pid):
-	'''
-	Get the current CPU processor time as a double based on a process
-	instance (chrome#10).
-	'''
-
-	process = getProcessInstance(pid)
-	
-	try:
-		if process == None:
-			print "getProcessCpuTimeWindows: process is null"
-			return None
-		
-		#if cpu_path == None:
-		cpu_path = win32pdh.MakeCounterPath( (None, 'Process', process, None, 0, '% Processor Time') )
-		
-		#if cpu_hq == None:
-		cpu_hq = win32pdh.OpenQuery()
-		
-		#if cpu_counter_handle == None:
-		cpu_counter_handle = win32pdh.AddCounter(cpu_hq, cpu_path) #convert counter path to counter handle
-		win32pdh.CollectQueryData(cpu_hq) #collects data for the counter
-		time.sleep(0.25)
-		
-		win32pdh.CollectQueryData(cpu_hq) #collects data for the counter
-		(v,cpu) = win32pdh.GetFormattedCounterValue(cpu_counter_handle, win32pdh.PDH_FMT_DOUBLE)
-		return cpu
-	
-	except:
-		print "getProcessCpuTimeWindows threw exception!"
-		print sys.exc_info()
-	
-	return None
-
-def getProcessInstance(pid):
-	'''
-	Get the process instance name using pid.
-	'''
-	
-	hq = None
-	counter_handle = None
-	
-	try:
-		
-		win32pdh.EnumObjects(None, None, win32pdh.PERF_DETAIL_WIZARD)
-		junk, instances = win32pdh.EnumObjectItems(None,None,'Process', win32pdh.PERF_DETAIL_WIZARD)
-	
-		proc_dict = {}
-		for instance in instances:
-			if proc_dict.has_key(instance):
-				proc_dict[instance] = proc_dict[instance] + 1
-			else:
-				proc_dict[instance]=0
-		
-		proc_ids = []
-		for instance, max_instances in proc_dict.items():
-			for inum in xrange(max_instances+1):
-				hq = win32pdh.OpenQuery() # initializes the query handle 
-				try:
-					path = win32pdh.MakeCounterPath( (None, 'Process', instance, None, inum, 'ID Process') )
-					counter_handle=win32pdh.AddCounter(hq, path) #convert counter path to counter handle
-					try:
-						win32pdh.CollectQueryData(hq) #collects data for the counter 
-						type, val = win32pdh.GetFormattedCounterValue(counter_handle, win32pdh.PDH_FMT_LONG)
-						proc_ids.append((instance, val))
-						
-						if val == pid:
-							return "%s#%d" % (instance, inum)
-						
-					except win32pdh.error, e:
-						#print e
-						pass
-					
-					win32pdh.RemoveCounter(counter_handle)
-					counter_handle = None
-				
-				except win32pdh.error, e:
-					#print e
-					pass
-				win32pdh.CloseQuery(hq)
-				hq = None
-	except:
-		print "getProcessInstance thew exception"
-		print sys.exc_info()
-	
-	finally:
+	def clean(self):
+		if self._traceFolder != None:
+			shutil.rmtree(self._traceFolder)
+			self._traceFolder = None
 		
 		try:
-			if counter_handle != None:
-				win32pdh.RemoveCounter(counter_handle)
-				counter_handle = None
-			if hq != None:
-				win32pdh.CloseQuery(hq)
-				hq = None
+			os.unlink("bblocks.out")
 		except:
 			pass
 	
-	# SHouldn't get here...we hope!
-	return None
+	def delta(self, list1, list2):
+		'''
+		Find the delta of two lists
+		'''
 		
-def getCoverage(cmd):
-	'''
-	returns array of offsets
-	'''
+		d = []
+		
+		for i in list1:
+			if i not in list2:
+				d.append(i)
+		
+		return d
 	
-	try:
-		os.unlink("bblocks.out")
-	except:
-		pass
+	def stripPath(self, fileName):
+		'''
+		Strip the path from a filename
+		'''
+		
+		indx = fileName.rfind(os.path.sep)
+		
+		if indx == -1:
+			return fileName
+		
+		return fileName[indx+1:]
 	
-	if needsKilling:
+	def getProcessEntry(self, pid):
+		'''
+		Return the PROCESSENTRY32 struct for
+		a process id.
+		'''
 		
-		if not isWindows():
-			raise Exception("-gui only on Windows for now")
+		# See http://msdn2.microsoft.com/en-us/library/ms686701.aspx
+		CreateToolhelp32Snapshot = ctypes.windll.kernel32.CreateToolhelp32Snapshot
+		Process32First = ctypes.windll.kernel32.Process32First
+		Process32Next = ctypes.windll.kernel32.Process32Next
+		CloseHandle = ctypes.windll.kernel32.CloseHandle
 		
-		hProcPin = os.spawnl(os.P_NOWAIT, 
-			"pin-2.8-37300-msvc10-ia32_intel64-windows\\ia32\\bin\\pin.exe", 
-			"pin-2.8-37300-msvc10-ia32_intel64-windows\\ia32\\bin\\pin.exe", 
-			"-t", 
-			"bblocks.dll", 
-			"--", 
-			cmd)
+		hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0)
 		
-		time.sleep(2)
+		pe32 = PROCESSENTRY32()
+		pe32.dwSize = ctypes.sizeof(PROCESSENTRY32)
 		
-		pid = ctypes.windll.kernel32.GetProcessId(hProcPin)
-		for childPid in process_list(pid):
-			pass
-		
-		print "parentpid:", pid, "child pid:", childPid
+		if Process32First(hProcessSnap, ctypes.byref(pe32)) == win32con.FALSE:
+			print >> sys.stderr, "Failed getting first process."
+			return
 		
 		while True:
-			cpu = getProcessCpuTime(childPid)
-			if cpu == None:
-				print "CPU IS None!"
-				sys.exit(0)
-			
-			if cpu != None and cpu < 0.5:
-				print "Kill due to CPU time"
-				
-				os.system("taskkill /pid %d" % (childPid))
-				os.system("taskkill /f /pid %d" % (childPid))
-				
-				# Prevent Zombies!
-				os.waitpid(hProcPin, 0)
+			if pe32.th32ProcessID == pid:
 				break
 			
-			time.sleep(0.50)
-	else:
-		pid = os.spawnl(os.P_WAIT, 
-			"pin-2.8-37300-msvc10-ia32_intel64-windows\\ia32\\bin\\pin.exe", 
-			"pin-2.8-37300-msvc10-ia32_intel64-windows\\ia32\\bin\\pin.exe", 
-			"-t", 
-			"bblocks.dll", 
-			"--", 
-			cmd)
-
-	
-	fd = open("bblocks.out", "rb+")
-	strOffsets = fd.read().replace("\r", "").split("\n")
-	offsets = []
-	fd.close()
-	
-	for s in strOffsets:
-		if len(s) < 2:
-			continue
+			if Process32Next(hProcessSnap, ctypes.byref(pe32)) == win32con.FALSE:
+				pe32 = None
+				break
 		
-		if len(s) > 8:
-			print "len(s)", len(s)
-			continue
-		offsets.append(int(s, 16))
+		CloseHandle(hProcessSnap)
+		
+		return pe32
 	
-	try:
-		os.unlink("bblocks.out")
-	except:
-		pass
+	def process_list(self, parentId):
+		'''
+		Yield all child process ids
+		'''
+		
+		# See http://msdn2.microsoft.com/en-us/library/ms686701.aspx
+		CreateToolhelp32Snapshot = ctypes.windll.kernel32.CreateToolhelp32Snapshot
+		Process32First = ctypes.windll.kernel32.Process32First
+		Process32Next = ctypes.windll.kernel32.Process32Next
+		CloseHandle = ctypes.windll.kernel32.CloseHandle
+		
+		hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0)
+		
+		pe32 = PROCESSENTRY32()
+		pe32.dwSize = ctypes.sizeof(PROCESSENTRY32)
+		
+		if Process32First(hProcessSnap, ctypes.byref(pe32)) == win32con.FALSE:
+			print >> sys.stderr, "Failed getting first process."
+			return
+		
+		while True:
+			if pe32.th32ParentProcessID == parentId:
+				yield pe32.th32ProcessID
+			
+			if Process32Next(hProcessSnap, ctypes.byref(pe32)) == win32con.FALSE:
+				break
+		
+		CloseHandle(hProcessSnap)
 	
-	return offsets
+	def isWindows(self):
+		'''
+		Is this a windows system?
+		'''
+		
+		if sys.platform == 'win32':
+			return True
+		
+		return False
+	
+	def getProcessCpuTime(self, pid):
+		'''
+		Get the current CPU processor time as a double based on a process
+		instance (chrome#10).
+		'''
+	
+		process = self.getProcessInstance(pid)
+		
+		try:
+			if process == None:
+				print "getProcessCpuTimeWindows: process is null"
+				return None
+			
+			#if cpu_path == None:
+			cpu_path = win32pdh.MakeCounterPath( (None, 'Process', process, None, 0, '% Processor Time') )
+			
+			#if cpu_hq == None:
+			cpu_hq = win32pdh.OpenQuery()
+			
+			#if cpu_counter_handle == None:
+			cpu_counter_handle = win32pdh.AddCounter(cpu_hq, cpu_path) #convert counter path to counter handle
+			win32pdh.CollectQueryData(cpu_hq) #collects data for the counter
+			time.sleep(0.25)
+			
+			win32pdh.CollectQueryData(cpu_hq) #collects data for the counter
+			(v,cpu) = win32pdh.GetFormattedCounterValue(cpu_counter_handle, win32pdh.PDH_FMT_DOUBLE)
+			return cpu
+		
+		except:
+			print "getProcessCpuTimeWindows threw exception!"
+			print sys.exc_info()
+		
+		return None
+	
+	def getProcessInstance(self, pid):
+		'''
+		Get the process instance name using pid.
+		'''
+		
+		hq = None
+		counter_handle = None
+		
+		try:
+			
+			win32pdh.EnumObjects(None, None, win32pdh.PERF_DETAIL_WIZARD)
+			junk, instances = win32pdh.EnumObjectItems(None,None,'Process', win32pdh.PERF_DETAIL_WIZARD)
+		
+			proc_dict = {}
+			for instance in instances:
+				if proc_dict.has_key(instance):
+					proc_dict[instance] = proc_dict[instance] + 1
+				else:
+					proc_dict[instance]=0
+			
+			proc_ids = []
+			for instance, max_instances in proc_dict.items():
+				for inum in xrange(max_instances+1):
+					hq = win32pdh.OpenQuery() # initializes the query handle 
+					try:
+						path = win32pdh.MakeCounterPath( (None, 'Process', instance, None, inum, 'ID Process') )
+						counter_handle=win32pdh.AddCounter(hq, path) #convert counter path to counter handle
+						try:
+							win32pdh.CollectQueryData(hq) #collects data for the counter 
+							type, val = win32pdh.GetFormattedCounterValue(counter_handle, win32pdh.PDH_FMT_LONG)
+							proc_ids.append((instance, val))
+							
+							if val == pid:
+								return "%s#%d" % (instance, inum)
+							
+						except win32pdh.error, e:
+							#print e
+							pass
+						
+						win32pdh.RemoveCounter(counter_handle)
+						counter_handle = None
+					
+					except win32pdh.error, e:
+						#print e
+						pass
+					win32pdh.CloseQuery(hq)
+					hq = None
+		except:
+			print "getProcessInstance thew exception"
+			print sys.exc_info()
+		
+		finally:
+			
+			try:
+				if counter_handle != None:
+					win32pdh.RemoveCounter(counter_handle)
+					counter_handle = None
+				if hq != None:
+					win32pdh.CloseQuery(hq)
+					hq = None
+			except:
+				pass
+		
+		# SHouldn't get here...we hope!
+		return None
+	
+	def fileLineCount(self, fname):
+		f = open(fname)
+		for i, l in enumerate(f):
+			pass
+		
+		f.close()
+		return i + 1
+	
+	def loadBlocks(self, sampleFile):
+		bblocks = []
+		f = open(os.path.join(self._traceFolder, self.stripPath(sampleFile)+".trace"))
+		for i,l in enumerate(f):
+			bblocks.append(l)
+		
+		f.close()
+		
+		return bblocks
+	
+	def getCoverage(self, cmd, sampleFile):
+		'''
+		Generates a trace into "sampleFile.trace" and returns
+		the count of basic blocks hit.
+		'''
+		
+		try:
+			os.unlink("bblocks.out")
+		except:
+			pass
+		
+		if self._traceFolder == None:
+			self._traceFolder = tempfile.mkdtemp()
+		
+		if needsKilling:
+			
+			if not self.isWindows():
+				raise Exception("-gui only on Windows for now")
+			
+			hProcPin = os.spawnl(os.P_NOWAIT, 
+				"pin-2.8-37300-msvc10-ia32_intel64-windows\\ia32\\bin\\pin.exe", 
+				"pin-2.8-37300-msvc10-ia32_intel64-windows\\ia32\\bin\\pin.exe", 
+				"-t", 
+				"bblocks.dll", 
+				"--", 
+				cmd)
+			
+			time.sleep(2)
+			
+			pid = ctypes.windll.kernel32.GetProcessId(hProcPin)
+			for childPid in process_list(pid):
+				pass
+			
+			print "parentpid:", pid, "child pid:", childPid
+			
+			while True:
+				cpu = getProcessCpuTime(childPid)
+				if cpu == None:
+					print "CPU IS None!"
+					sys.exit(0)
+				
+				if cpu != None and cpu < 0.5:
+					print "Kill due to CPU time"
+					
+					os.system("taskkill /pid %d" % (childPid))
+					os.system("taskkill /f /pid %d" % (childPid))
+					
+					# Prevent Zombies!
+					os.waitpid(hProcPin, 0)
+					break
+				
+				time.sleep(0.50)
+		else:
+			pid = os.spawnl(os.P_WAIT, 
+				"pin-2.8-37300-msvc10-ia32_intel64-windows\\ia32\\bin\\pin.exe", 
+				"pin-2.8-37300-msvc10-ia32_intel64-windows\\ia32\\bin\\pin.exe", 
+				"-t", 
+				"bblocks.dll", 
+				"--", 
+				cmd)
+		
+		
+		# For large traces we shouldn't try and keep offsets
+		# in memory.  Lets just copy our trace to a temp file
+		# for use later.
+		
+		bblCount = self.fileLineCount("bblocks.out")
+		shutil.move("bblocks.out", os.path.join(self._traceFolder, self.stripPath(sampleFile)+".trace"))
+		
+		return bblCount
+		
+	
+	def runCoverage(self, command, samplesGlob, minsetPath, needsKilling):
+		'''
+		Main entry point
+		'''
+		
+		try:
+			
+			print "[*] Finding all basic blocks"
+			
+			sampleFiles = []
+			for f in glob.glob(samplesGlob):
+				if os.path.isdir(f):
+					continue
+				
+				sampleFiles.append(f)
+			
+			print "[*] Found %d sample files" % len(sampleFiles)
+			
+			#: Dictionary of lists, key is sampleFile
+			bblocks = {}
+			#: Most coverage
+			sampleFileMostCoverage = None
+			sampleFileMostCoverageCount = -1
+			#: Min set
+			minset = []
+			
+			for sampleFile in sampleFiles:
+				print "[*] Determining coverage with [%s]...." % sampleFile
+				
+				cmd = command % sampleFile
+				bblocks[sampleFile] = self.getCoverage(cmd, sampleFile)
+				
+				print "[-] %s hit %d blocks" % (sampleFile, bblocks[sampleFile])
+				
+			
+			# Figure out who covered the most blocks
+			for sampleFile in sampleFiles:
+				cnt = bblocks[sampleFile]
+				if cnt > sampleFileMostCoverageCount:
+					sampleFileMostCoverage = sampleFile
+					sampleFileMostCoverageCount = cnt
+			
+			minset.append(sampleFileMostCoverage)
+			
+			print ""
+			print "[*] Master template is [%s] with a coverage of %d blocks" % (sampleFileMostCoverage, sampleFileMostCoverageCount)
+			
+			# Start with file with most coverage
+			coveredBlocks = self.loadBlocks(sampleFileMostCoverage)
+			
+			for sampleFile in sampleFiles:
+				
+				# Don't repeat master file
+				if sampleFile == sampleFileMostCoverage:
+					continue
+				
+				bblTrace = self.loadBlocks(sampleFile)
+				d = self.delta(bblTrace, coveredBlocks)
+				bblTrace = None
+				
+				if len(d) > 0:
+					minset.append(sampleFile)
+					for b in d:
+						coveredBlocks.append(b)
+			
+			# No longer need these
+			coveredBlocks = None
+			d = None
+			
+			print ""
+			print "[*] Minimum set is %d of %d files:" % (len(minset), len(sampleFiles))
+			
+			try:
+				os.mkdir(minsetPath)
+			except:
+				pass
+			
+			for sampleFile in minset:
+				print "[-]    %s" % sampleFile
+				shutil.copyfile(sampleFile, os.path.join(minsetPath, self.stripPath(sampleFile)))
+			
+			print "\n"
+		
+		finally:
+			# Clean up after ourselvs
+			self.clean()
+
 
 print ""
-print "] Peach Minset Finder v0.7"
+print "] Peach Minset Finder v0.9"
 print "] Copyright (c) Michael Eddington\n"
 
 if len(sys.argv) < 4:
@@ -336,124 +476,22 @@ if len(sys.argv) < 4:
 	
 	sys.exit(0)
 
-# 1. Read in offsets
-
-try:
-	os.unlink("bblocks.out")
-except:
-	pass
-
 needsKilling = False
 if sys.argv[1] == "-gui":
 	needsKilling = True
-	samples = sys.argv[2]
+	samplesGlob = sys.argv[2]
 	minsetPath = sys.argv[3]
 	command = sys.argv[4]
 	if len(sys.argv) > 5:
 		command = '"' + command + '" ' + sys.argv[5]
 else:
-	samples = sys.argv[1]
+	samplesGlob = sys.argv[1]
 	minsetPath = sys.argv[2]
 	command = sys.argv[3]
 	if len(sys.argv) > 4:
 		command = '"' + command + '" ' + sys.argv[4]
 
-print "[*] Finding all basic blocks"
-
-# Locate the base path of this executable/script
-p = None
-if not (hasattr(sys,"frozen") and sys.frozen == "console_exe"):
-	p = __file__[:-9]
-
-else:
-	p = os.path.dirname(os.path.abspath(sys.executable))
-
-if len(p) == 0:
-	p = "."
-
-sampleFiles = []
-for f in glob.glob(samples):
-	if os.path.isdir(f):
-		continue
-	
-	sampleFiles.append(f)
-
-print "[*] Found %d sample files" % len(sampleFiles)
-
-#: Dictionary of lists, key is sampleFile
-bblocks = {}
-#: Most coverage
-sampleFileMostCoverage = None
-sampleFileMostCoverageCount = -1
-#: Min set
-minset = []
-
-for sampleFile in sampleFiles:
-	print "[*] Determining coverage with [%s]...." % sampleFile
-	
-	cmd = command % sampleFile
-	bblocks[sampleFile] = sorted(set(getCoverage(cmd)))
-	
-	print "[-] %s hit %d blocks" % (sampleFile, len(bblocks[sampleFile]))
-	
-
-# Figure out who covered the most blocks
-for sampleFile in sampleFiles:
-	cnt = len(bblocks[sampleFile])
-	if cnt > sampleFileMostCoverageCount:
-		sampleFileMostCoverage = sampleFile
-		sampleFileMostCoverageCount = cnt
-
-minset.append(sampleFileMostCoverage)
-
-print ""
-print "[*] Master template is [%s] with a coverage of %d blocks" % (sampleFileMostCoverage, sampleFileMostCoverageCount)
-
-def delta(list1, list2):
-	'''
-	Find the delta of two lists
-	'''
-	
-	d = []
-	
-	for i in list1:
-		if i not in list2:
-			d.append(i)
-	
-	return d
-
-def stripPath(fileName):
-	indx = fileName.rfind(os.path.sep)
-	
-	if indx == -1:
-		return fileName
-	
-	return fileName[indx+1:]
-
-coveredBlocks = []
-for b in bblocks[sampleFileMostCoverage]:
-	coveredBlocks.append(b)
-
-for sampleFile in sampleFiles:
-	d = delta(bblocks[sampleFile], coveredBlocks)
-	
-	if len(d) > 0:
-		minset.append(sampleFile)
-		for b in d:
-			coveredBlocks.append(b)
-
-print ""
-print "[*] Minimum set is %d of %d files:" % (len(minset), len(sampleFiles))
-
-try:
-	os.mkdir(minsetPath)
-except:
-	pass
-
-for sampleFile in minset:
-	print "[-]    %s" % sampleFile
-	shutil.copyfile(sampleFile, os.path.join(minsetPath, stripPath(sampleFile)))
-
-print "\n"
+coverage = PeachCoverage()
+coverage.runCoverage(command, samplesGlob, minsetPath, needsKilling)
 
 # end
