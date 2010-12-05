0dc64a219f678bd4d21a8dbaeb807ee01060f994
meddingt <meddingt@bf843fa0-5fda-4187-82bf-2edb81848ee6>
2010-12-05 07:42:35 +0000

    git-svn-id: svn://svn.code.sf.net/p/peachfuzz/svn/branches/Peach2.3@2233 bf843fa0-5fda-4187-82bf-2edb81848ee6

--- peachrand.sh
+++ peachrand.sh
@@ -1,9 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 BASEDIR=`dirname $0`
-if [ ${#BASEDIR} == 0 ]
-    then python peach.py --strategy=rand.RandomMutationStrategy $1 $2 $3 $4 $5 $6 $7 $8 $9
-    else python $BASEDIR/peach.py --strategy=rand.RandomMutationStrategy $1 $2 $3 $4 $5 $6 $7 $8 $9
+if [ "${#BASEDIR}" == "0" ]; then
+    python peach.py --strategy=rand.RandomMutationStrategy $1 $2 $3 $4 $5 $6 $7 $8 $9
+else
+    python $BASEDIR/peach.py --strategy=rand.RandomMutationStrategy $1 $2 $3 $4 $5 $6 $7 $8 $9
 fi
-
-
