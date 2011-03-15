93a33105e655198badf53ed2369dce74b82bb748
meddingt <meddingt@bf843fa0-5fda-4187-82bf-2edb81848ee6>
2011-03-15 02:07:24 +0000

    git-svn-id: svn://svn.code.sf.net/p/peachfuzz/svn/branches/Peach2.3@2297 bf843fa0-5fda-4187-82bf-2edb81848ee6

--- dependencies/py2.5-win32/install.bat
+++ dependencies/py2.5-win32/install.bat
@@ -12,6 +12,5 @@ msiexec /qn /i comtypes-0.6.1.win32-py2.5.msi
 msiexec /qn /i PyDbgEng-0.14.win32-py2.5.msi
 multiprocessing-2.6.2.1.win32-py2.5.exe
 wxPython2.8-win32-ansi-2.8.7.1-py25.exe
-msiexec /qn /i wxPropertyGrid-1.2.11.win32-py2.5.msi
 WinPcap_4_0_2.exe
 psutil-0.2.0.win32-py2.5.exe
